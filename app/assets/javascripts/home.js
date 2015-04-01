f447f01c0693e3324acdc6d4ae731be8f4ca4250
Konstantin Klitenik <konstantin@bytelight.com>
2015-04-01 14:50:35 -0400

    Styled current goal partial

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,31 @@
 //= require nike_goals
+//= require progressbar
 
 $(function() {
     if ($(".home.index").length) {
         chartGoalBurndown(true);
+        drawProgressBars();
     }
 });
+
+function drawProgressBars() {
+    var circle = new ProgressBar.Circle('#progress-circle', {
+        color: '#1BFF6C',
+        strokeWidth: 8,
+        trailWidth: 8,
+        //fill: '#eee',
+        text: {
+            value: '0%',
+            color: '#777',
+            autoStyle: true
+        },
+        duration: 1000,
+        easing: 'easeOut',
+        step: function(state, bar) {
+            bar.setText((bar.value() * 100).toFixed(0) + "%");
+        }
+    });
+
+    var progress = $('#progress-circle').data("progress")
+    circle.animate(progress);
+}
