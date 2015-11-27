0955229946b84760bf1119fa792b030adf1f1104
Konstantin Klitenik <konstantin@bytelight.com>
2015-11-27 12:56:59 -0500

    Catch more exceptions

--- lib/nike_sync.rb
+++ lib/nike_sync.rb
@@ -13,18 +13,25 @@ class NikeSync
       nike = NikeApi.new(username: Rails.application.secrets.nike_user_name,
                          password: Rails.application.secrets.nike_password)
     rescue Exception => e
-      Rails.logger.info("Post error: #{e.message}")
+      Rails.logger.info("Login error: #{e.message}")
       puts "Failed to get user"
       return -1
     end
 
     latest_run = NikeRun.last
 
-    if latest_run.present?
-      runs = nike.get_activity_list_json_with_dates(start_date: NikeRun.last.start_time, end_date: Date.today)
-    else
-      runs = nike.get_activity_list_json_with_count(count: 9999)
+    begin
+      if latest_run.present?
+        runs = nike.get_activity_list_json_with_dates(start_date: NikeRun.last.start_time, end_date: Date.today)
+      else
+        runs = nike.get_activity_list_json_with_count(count: 9999)
+      end
+    rescue
+        Rails.logger.info("Get runs error: #{e.message}")
+        puts "Failed to get runs"
+        return -1
     end
+
 
     if runs.nil?
       new_run_count = -1    # in case of error, return -1
