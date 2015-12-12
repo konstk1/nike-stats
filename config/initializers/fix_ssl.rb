388f8852e08376a18d0a260285ee0db049405c9d
Konstantin Klitenik <konstantin@bytelight.com>
2015-12-12 16:57:58 -0500

    Use TLS 1.2

--- config/initializers/fix_ssl.rb
+++ config/initializers/fix_ssl.rb
@@ -1,19 +1,26 @@
-# require 'open-uri'
-# require 'net/https'
-#
-# module Net
-#   class HTTP
-#     alias_method :original_use_ssl=, :use_ssl=
-#
-#     def use_ssl=(flag)
-#       # path = ( Rails.env == "development") ? "lib/ca-bundle.crt" : "/usr/lib/ssl/certs/ca-certificates.crt"
-#       # if Rails.env == "production"
-#         path = 'lib/ca-bundle.crt'
-#         self.ca_file = Rails.root.join(path).to_s
-#         self.verify_mode = OpenSSL::SSL::VERIFY_PEER
-#         self.original_use_ssl = flag
-#       # end
-#
-#     end
-#   end
-# end
+require 'open-uri'
+require 'net/https'
+
+module Net
+  class HTTP
+    alias_method :original_use_ssl=, :use_ssl=
+
+    @original_ca_file
+
+    def use_ssl=(flag)
+      # path = ( Rails.env == "development") ? "lib/ca-bundle.crt" : "/usr/lib/ssl/certs/ca-certificates.crt"
+      # if Rails.env == "production"
+        path = 'lib/ca-bundle.crt'
+        @original_ca_file = self.ca_file
+        self.ca_file = Rails.root.join(path).to_s
+        self.verify_mode = OpenSSL::SSL::VERIFY_PEER
+        self.ssl_version = :TLSv1_2
+        self.original_use_ssl = flag
+      # end
+    end
+
+    def restore_ca_file
+      self.ca_file = @original_ca_file
+    end
+  end
+end
