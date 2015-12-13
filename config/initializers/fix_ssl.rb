5da32bdb67b22dc375467ed3c4d94fae49efeefc
Konstantin Klitenik <konstantin@bytelight.com>
2015-12-13 16:06:06 -0500

    Using new cert file

--- config/initializers/fix_ssl.rb
+++ config/initializers/fix_ssl.rb
@@ -5,22 +5,15 @@ module Net
   class HTTP
     alias_method :original_use_ssl=, :use_ssl=
 
-    @original_ca_file
-
     def use_ssl=(flag)
       # path = ( Rails.env == "development") ? "lib/ca-bundle.crt" : "/usr/lib/ssl/certs/ca-certificates.crt"
       # if Rails.env == "production"
-        path = 'lib/ca-bundle.crt'
-        @original_ca_file = self.ca_file
+        path = 'lib/ca-bundle.pem'
         self.ca_file = Rails.root.join(path).to_s
         self.verify_mode = OpenSSL::SSL::VERIFY_PEER
-        self.ssl_version = :TLSv1_2
+        self.ssl_version = :TLSv1
         self.original_use_ssl = flag
       # end
-    end
-
-    def restore_ca_file
-      self.ca_file = @original_ca_file
     end
   end
 end
